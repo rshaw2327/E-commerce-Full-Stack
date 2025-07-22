ad7d506f34f0a4a94ecf8c97e73dcb0b202a7726
rshaw2327 <rshaw2327@gmail.com>
2025-07-22 13:09:14 +0100

    Feat: frontend url edit to cors

--- ecommerceBackend/app.js
+++ ecommerceBackend/app.js
@@ -3,7 +3,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const cors = require("cors")
 
-const whitelist=["http://127.0.0.1:5502","http://127.0.0.1:5501"] 
+const whitelist=["http://127.0.0.1:5502","http://127.0.0.1:5501","e-commerce-full-stack-zeta.vercel.app","e-commerce-full-stack-2rfq.vercel.app"] 
 const corsOptions={
   origin:function(origin,callback){
     if(!origin || whitelist.includes(origin)){
